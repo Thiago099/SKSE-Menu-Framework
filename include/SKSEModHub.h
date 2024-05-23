c2f154e4c41608c5f5c76de18ba9d2a0e4b19f14
Thiago Kaique <66787043+Thiago099@users.noreply.github.com>
2024-05-23 09:15:58 -0300

    refactory

--- include/SKSEModHub.h
+++ include/SKSEModHub.h
@@ -1,10 +1,36 @@
 #pragma once
 #include <imgui/imgui.h>
-#include "Application.h"
-#include "Renderer.h"
+
+#ifdef IS_HOST_PLUGIN
+    #include "Application.h"
+    #include "Renderer.h"
+#endif
+
+#ifdef IS_HOST_PLUGIN
+    #define FUNCTION_PREFIX extern "C" [[maybe_unused]] __declspec(dllexport)
+#else
+    #define FUNCTION_PREFIX extern "C" __declspec(dllimport)
+    typedef void(__stdcall* RenderFunction)();
+#endif
+
+#ifdef IS_HOST_PLUGIN
+FUNCTION_PREFIX void AddSection(const char* path, RenderFunction rendererFunction);
+FUNCTION_PREFIX void AddWindow(const char* key, ImGuiWindowFlags flags, std::function<void()> const& rendererFunction);
+#else
+    namespace SKSEModHubInternal {
+        inline std::string key;
+
+        FUNCTION_PREFIX void AddSection(const char* path, RenderFunction rendererFunction);
+    }
+    namespace SKSEModHub {
+        FUNCTION_PREFIX void AddWindow(const char* key, ImGuiWindowFlags flags,
+                                       std::function<void()> const& rendererFunction);
 
 
-#define EXPORT_FUNCTION extern "C" [[maybe_unused]] __declspec(dllexport)
+        inline void AddSection(std::string menu, RenderFunction rendererFunction) {
+            SKSEModHubInternal::AddSection((SKSEModHubInternal::key + "/" + menu).c_str(), rendererFunction);
+        }
 
-EXPORT_FUNCTION void AddSection(const char* path, RenderFunction rendererFunction);
-EXPORT_FUNCTION void AddWindow(const char* key, ImGuiWindowFlags flags, std::function<void()> const& rendererFunction);
+        inline void Init(std::string key) { SKSEModHubInternal::key = key; }
+    }
+#endif
