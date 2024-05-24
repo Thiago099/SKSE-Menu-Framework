c302f38ff339efe4557fc17f6ed9bd542b48559c
Thiago Kaique <66787043+Thiago099@users.noreply.github.com>
2024-05-23 21:57:39 -0300

    removed unused code

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,21 +1,12 @@
 #include "Plugin.h"
 
-void OnMessage(SKSE::MessagingInterface::Message* message) {
-    if (message->type == SKSE::MessagingInterface::kDataLoaded) {
-        // Start
-    }
-    if (message->type == SKSE::MessagingInterface::kPostLoadGame) {
-        // Post-load
-    }
-}
+
 SKSEPluginLoad(const SKSE::LoadInterface *skse) {
 
     SetupLog();
     logger::info("Plugin loaded");
     SKSE::Init(skse);
     Config::Init();
-    SKSE::GetMessagingInterface()->RegisterListener(OnMessage);
-
     auto builder = ImGui::Renderer::GetBuilder();
     builder->Install();
     delete builder;
